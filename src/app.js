8210dfccf2853795cc9f50eac05c301b43243806
Shubham Raj <shubhamxraj10@gmail.com>
2024-11-01 02:20:41 +0530

    using environment variable for deployment

--- src/app.js
+++ src/app.js
@@ -2,27 +2,22 @@ import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 
-import { readFile } from "fs/promises";
-import { fileURLToPath } from "url";
-import { dirname } from "path";
 import admin from "firebase-admin";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-// Load service account
-const serviceAccount = JSON.parse(
-  await readFile(
-    new URL(
-      "../config/calevent-2fc84-firebase-adminsdk-4rggx-f28eea9dae.json",
-      import.meta.url
-    )
-  )
-);
 
 // Initialize Firebase
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert({
+    type: "service_account",
+    project_id: process.env.FIREBASE_PROJECT_ID,
+    private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+    private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+    client_email: process.env.FIREBASE_CLIENT_EMAIL,
+    client_id: process.env.FIREBASE_CLIENT_ID,
+    auth_uri: "https://accounts.google.com/o/oauth2/auth",
+    token_uri: "https://oauth2.googleapis.com/token",
+    auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+    client_x509_cert_url: process.env.FIREBASE_CERT_URL,
+  }),
 });
 
 const app = express();
